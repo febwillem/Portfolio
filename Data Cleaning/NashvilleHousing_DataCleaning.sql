f863193f4d6a6389a1b9dc005ec111a78a2647ba
Febrizio Willem Ong <85860181+febwillem@users.noreply.github.com>
2021-06-20 10:07:21 +0700

    Add files via upload

---
+++ Data Cleaning/NashvilleHousing_DataCleaning.sql
@@ -0,0 +1,192 @@
+/*
+Data Cleaning with SQL
+*/
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+--------------------------------------------------------------
+-- Standarize Date format
+
+SELECT SaleDate, CONVERT(DATE, SaleDate)
+FROM PortfolioProject..NashvilleHousing
+
+UPDATE PortfolioProject..NashvilleHousing
+SET SaleDate = CONVERT(DATE, SaleDate)
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD SaleDateConverted DATE;
+
+UPDATE PortfolioProject..NashvilleHousing
+SET SaleDateConverted = CONVERT(DATE, SaleDate)
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+--------------------------------------------------------------
+-- Populate Property Address Data
+
+SELECT PropertyAddress
+FROM PortfolioProject..NashvilleHousing
+WHERE PropertyAddress IS NULL
+
+SELECT a.ParcelID
+, a.PropertyAddress
+, b.ParcelID
+, b.PropertyAddress
+, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing a
+JOIN PortfolioProject..NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing a
+JOIN PortfolioProject..NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+--------------------------------------------------------------
+-- Breaking out address into individual columns (Address. City, State)
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)) AS Address
+FROM PortfolioProject..NashvilleHousing
+-- We substract the Address from the property address without the city
+-- But we still have the comma after the address name
+-- We can use thix syntax to remove the comma
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) AS Address
+FROM PortfolioProject..NashvilleHousing
+
+-- Now add the city
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) AS Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress)) AS City
+FROM PortfolioProject..NashvilleHousing
+
+-- Update the table
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD PropertySplitAddress nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1)
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD PropertySplitCity nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress))
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+-- Look at the Owner Address
+
+SELECT OwnerAddress
+FROM PortfolioProject..NashvilleHousing
+
+-- Now we're going to split the owner address
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+FROM PortfolioProject..NashvilleHousing
+-- We got the state
+-- Now let's split the address and the city
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress,',','.'),3) AS Address
+, PARSENAME(REPLACE(OwnerAddress,',','.'),2) AS City
+, PARSENAME(REPLACE(OwnerAddress,',','.'),1) AS State
+FROM PortfolioProject..NashvilleHousing
+
+-- Update the table
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD OwnerSplitAddress nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress,',','.'),3)
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD OwnerSplitCity nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress,',','.'),2)
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD OwnerSplitState nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+--------------------------------------------------------------
+-- Change Y and N to Yes and No in "Sold as Vacant" field
+
+SELECT DISTINCT(SoldAsVacant)
+, COUNT(SoldAsVacant)
+FROM PortfolioProject..NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+-- Let's change it
+
+SELECT SoldAsVacant
+, CASE
+	WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	WHEN SoldAsVacant = 'N' THEN 'No'
+	ELSE SoldAsVacant
+END
+FROM PortfolioProject..NashvilleHousing
+WHERE SoldAsVacant IN ('Y', 'N')
+
+-- Update the table
+
+UPDATE PortfolioProject..NashvilleHousing
+SET SoldAsVacant = CASE
+					WHEN SoldAsVacant = 'Y' THEN 'Yes'
+					WHEN SoldAsVacant = 'N' THEN 'No'
+					ELSE SoldAsVacant
+				   END
+
+--------------------------------------------------------------
+-- Remove duplicates
+
+WITH RowNumCTE AS (
+SELECT *
+, ROW_NUMBER() OVER (
+	PARTITION BY ParcelID
+				, PropertyAddress
+				, SalePrice
+				, SaleDate
+				, LegalReference
+				ORDER BY
+					UniqueID
+					) row_num
+FROM PortfolioProject..NashvilleHousing
+)
+SELECT *
+FROM RowNumCTE
+WHERE row_num > 1
+ORDER BY PropertyAddress
+
+--------------------------------------------------------------
+-- Delete unused columns
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+DROP COLUMN PropertyAddress
+, SaleDate
+, OwnerAddress
+, TaxDistrict
