a2640311031fc2f770a68a1d36f671a086565a27
Febrizio Willem Ong <85860181+febwillem@users.noreply.github.com>
2021-06-20 22:37:17 +0700

    Update NashvilleHousing_DataCleaning.sql

--- Data Cleaning/NashvilleHousing_DataCleaning.sql
+++ Data Cleaning/NashvilleHousing_DataCleaning.sql
@@ -177,7 +177,7 @@ FROM PortfolioProject..NashvilleHousing
 DELETE
 FROM RowNumCTE
 WHERE row_num > 1
-ORDER BY PropertyAddress
+
 
 --------------------------------------------------------------
 -- Delete unused columns
